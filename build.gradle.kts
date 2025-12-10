e8c3f23796f8c716f8c542fed3e00b79a41efced
5201314155 <2721479100@qq.com>
2025-12-10 20:54:23 +0800

    Add clean task for mobile build entry

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,7 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
